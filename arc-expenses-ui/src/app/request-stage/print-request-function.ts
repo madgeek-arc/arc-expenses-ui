11c834a83b2c4d0e9520b7bd2dca941e46e23078
myrto1984 <myrtokoukouli@gmail.com>
2018-12-19 18:54:55 +0200

    uncommented print button

--- arc-expenses-ui/src/app/request-stage/print-request-function.ts
+++ arc-expenses-ui/src/app/request-stage/print-request-function.ts
@@ -2,26 +2,32 @@ export function printRequestPage() {
     let printContents, popupWin;
     printContents = document.getElementById('printable-section').innerHTML;
     popupWin = window.open('', '_blank', 'top=0,left=0,height=500,width=800');
-    popupWin.document.open();
+    // popupWin.document.open();
     popupWin.document.write(`
       <html>
         <head>
           <style>
-            .newStyle {
-                display: inline-block;
-                background-color: #b2e7c7;
-            }
+            
           </style>
           <title>Αίτημα </title>
           <!-- UIkit CSS -->
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/css/uikit.min.css" />
 
-          <!-- UIkit JS -->
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit.min.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit-icons.min.js"></script>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+                integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"/>
+
+          <link rel="stylesheet" href="../../assets/css/theme.scss" />
+          <link rel="stylesheet" href="../../assets/css/theme.update.scss" />
+          <link rel="stylesheet" href="./request-stage.component.scss" />
+          <link rel="stylesheet" href="./stage-component.scss" />
+
         </head>
-    <body onload="window.print();window.close()">${printContents}</body>
+        <body onload="window.print();window.close()">
+            <div class="uk-margin">
+                ${printContents}
+            </div>
+        </body>
       </html>`
     );
-    popupWin.document.close();
+    // popupWin.document.close();
 }
