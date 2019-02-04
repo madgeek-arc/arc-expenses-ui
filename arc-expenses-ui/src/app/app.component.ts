a9fd72724014132d723a51c380c26644e306a0ab
myrto1984 <myrtokoukouli@gmail.com>
2019-02-04 17:31:22 +0200

    updated commit date

--- arc-expenses-ui/src/app/app.component.ts
+++ arc-expenses-ui/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit, OnDestroy {
               private authService: AuthenticationService) {
 
     // TODO: DON'T FORGET TO UPDATE BEFORE COMMIT!!!
-    console.log('committed on: 01-02-2019 15:30 pm');
+    console.log('committed on: 04-02-2019 17:30 pm');
 
     // hides the console logs in production
     if ( (environment.production === true) &&
