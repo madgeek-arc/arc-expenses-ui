65ded5e6d50db198d62186675a41f352d9ee9c0b
myrto1984 <myrtokoukouli@gmail.com>
2019-02-27 16:34:05 +0200

    changed stage3 loan field label

--- arc-expenses-ui/src/app/app.component.ts
+++ arc-expenses-ui/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit, OnDestroy {
               private authService: AuthenticationService) {
 
     // TODO: DON'T FORGET TO UPDATE BEFORE COMMIT!!!
-    console.log('committed on: 12-02-2019 01:00 pm');
+    console.log('committed on: 27-02-2019 04:30 pm');
 
     // hides the console logs in production
     if ( (environment.production === true) &&
